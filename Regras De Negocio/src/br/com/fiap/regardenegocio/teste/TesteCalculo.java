b18a1d2f34f9cf73fecd5702e6c899214344c615
Logon Aluno <logonrmlocal@fiap.com.br>
2022-03-30 10:18:06 -0300

    Criando e Desenvolvendo o Exercício

---
+++ Regras De Negocio/src/br/com/fiap/regardenegocio/teste/TesteCalculo.java
@@ -0,0 +1,39 @@
+package br.com.fiap.regardenegocio.teste;
+
+import java.util.Calendar;
+import java.util.Scanner;
+
+import br.com.fiap.regardenegocio.contrato.Contrato;
+
+public class TesteCalculo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Chamando o Calendar
+		Calendar Calendario = Calendar.getInstance();
+		
+		// Criando Objeto
+		Contrato Contract = new Contrato();
+		
+		// Criando o Scanner
+		Scanner Dados = new Scanner(System.in);
+		
+		// Entrada dos Dados
+		System.out.println("Informe o Número do Contrato");
+		Contract.setNumeroContrato(Dados.nextInt());
+		//Dados.nextInt(Contract.getNumeroContrato());
+		
+		System.out.println("Informe a Data desse Contrato");
+		
+		System.out.println("Digite o valor do Contrato");
+		Contract.setValorContrato(Dados.nextDouble());
+		
+		System.out.println("Digite a Quantidade de Parcelas");
+		Contract.setNumeroParcelas(Dados.nextInt());
+		
+		// Saída dos dados
+		System.out.println(Contract);
+		
+	}
+
+}
